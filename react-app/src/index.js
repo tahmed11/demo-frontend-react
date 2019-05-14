81ab8073f23f4fdc925936ba3b93ef5271b57a25
tahmed11 <tanvir.ahmed@outlook.com>
2019-05-14 15:12:13 +1000

    react-intro

---
+++ react-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+
+const element = <h1>Hello World</h1>; 
+
+ReactDOM.render(element, document.getElementById("root"));
